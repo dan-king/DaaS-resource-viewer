b62ee363cbb01a88f1cab35093ed868f4dac2921
Dan King <dan.king@vptech.io>
2020-01-27 10:46:30 -0800

    Read and display mock data from CSV file

--- routes/homepage.js
+++ routes/homepage.js
@@ -3,15 +3,18 @@ var router = express.Router()
 
 // Homepage
 router.get('/', function(req, res){
-    var message = 'Hello World'
+    const csvFilePath='./public/csv/mock.csv'
+    const csv=require('csvtojson')
+    csv()
+        .fromFile(csvFilePath)
+        .then((data_content)=>{
+            var data_title = req.app.locals.mock_data_title
+            //data_content = req.app.locals.mock_data_content
+            res.render('home', {
+                data_title: data_title,
+                data_content: data_content
+            })
+        })
 
-    var data_title = req.app.locals.mock_data_title
-    var data_content = req.app.locals.mock_data_content
-
-    res.render('home', {
-        message: message,
-        data_title: data_title,
-        data_content: data_content
-    })
 })
 module.exports = router
